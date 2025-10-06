f2d154c51a1c8ca7240f9c72e7106f7e2f5e0af3
Chase <st10433939@vcconnect.edu.za>
2025-10-06 17:20:02 +0200

    Added Views

--- 10433939_CLDV6212_POE_P2/Program.cs
+++ 10433939_CLDV6212_POE_P2/Program.cs
@@ -1,29 +1,63 @@
-var builder = WebApplication.CreateBuilder(args);
+using _10433939_CLDV6212_POE_P2.Services;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace _10433939_CLDV6212_POE_P2.Models
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //Add services to the container
+            builder.Services.AddControllersWithViews();
+
+            //Register TableStorage with configuration
+            builder.Services.AddSingleton(new TableStorageService(configuration.GetConnectionString("AzureStorage")));
+
+            //Register Blob with configuration
+            builder.Services.AddSingleton(new BlobService(configuration.GetConnectionString("AzureStorage")));
+
+            //Register Queue with configuration
+            builder.Services.AddSingleton<QueueService>(sp =>
+            {
+                var connectionString = configuration.GetConnectionString("AzureStorage");
+                return new QueueService(connectionString, "orders");
+            });
+
+            //Register File with configuration
+            builder.Services.AddSingleton<AzureFileShareService>(sp =>
+            {
+                var connectionString = configuration.GetConnectionString("AzureStorage");
+                return new AzureFileShareService(connectionString, "productshare");
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
